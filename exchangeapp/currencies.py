51e49d94f10aa20ab5643373895ee561d800364d
Andri Jasinski <andri.jasinski@gmail.com>
2018-03-04 14:41:09 +0200

    fix

--- exchangeapp/currencies.py
+++ exchangeapp/currencies.py
@@ -1,7 +1,7 @@
 
 class Currencies():
     
-    currencies = ['eur', 'kzt', 'bob', 'rub']
+    currencies = ['eur', 'kzt', 'bob', 'rub', 'inr', 'gbp']
 
     def as_choices(self):
         return [(curr, curr.upper()) for curr in Currencies.currencies]
